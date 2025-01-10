e2ee6ea2c735b804cbbd4004bb0876cecf3530de
Pierre GD <pierre.gaudillere@gmail.com>
2025-01-10 09:46:48 +0100

    ui fixes

---
+++ Assets/Scripts/DragViewport.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragViewport : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public Graphic graphic;
+    public RectTransform container;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+}
