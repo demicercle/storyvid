20fcb5312ea9e2c3068cc67b49de0ccc0a372100
Pierre GD <pierre.gaudillere@gmail.com>
2025-01-22 10:59:45 +0100

    fixes and new data

--- Assets/Scripts/EpisodeVideoList.cs
+++ Assets/Scripts/EpisodeVideoList.cs
@@ -9,13 +9,21 @@ public class EpisodeVideoList : MonoBehaviour
     public int episodeIndex;
     public TMPro.TMP_Text header;
     public Button episodeButton;
-    public List<EpisodeVideoElement> videoElements;
     
+    private List<EpisodeVideoElement> videoElements;
     private GameManager gameManager;
 
     private void Awake()
     {
         gameManager = FindObjectOfType<GameManager>();
+
+        videoElements = new List<EpisodeVideoElement>();
+        videoElements.AddRange(GetComponentsInChildren<EpisodeVideoElement>());
+        
+        foreach (var episodeVideoElement in videoElements)
+        {
+            episodeVideoElement.gameObject.SetActive(false);
+        }
         
         episodeButton.onClick.AddListener(() =>
         {
@@ -26,15 +34,11 @@ public class EpisodeVideoList : MonoBehaviour
     private void Start()
     {
         header.text = "Episode " + episodeIndex.ToString();
-        
-        foreach (var episodeVideoElement in videoElements)
-        {
-            episodeVideoElement.gameObject.SetActive(false);
-        }
 
         var lastVideoElement = 0;
         foreach (string videoID in gameManager.GetVideoIDs(episodeIndex))
         {
+          //  Debug.Log(lastVideoElement + " " + videoID);
             videoElements[lastVideoElement].gameObject.SetActive(true);
             videoElements[lastVideoElement].episode = episodeIndex;
             videoElements[lastVideoElement].videoID = videoID;
