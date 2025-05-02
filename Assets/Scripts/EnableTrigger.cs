fa69b2d28f86d9a817ba188c9021058ab81e46c1
pierregd <pierre.gaudillere@gmail.com>
2025-05-02 16:48:46 +0200

    fix bouton retour

---
+++ Assets/Scripts/EnableTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnableTrigger : MonoBehaviour
+{
+        public UnityEvent onEnable, onDisable;
+
+        private void OnEnable()
+        {
+                onEnable.Invoke();
+        }
+
+        void OnDisable()
+        {
+                onDisable.Invoke();
+        }
+}
--- Assets/Scripts/PanelButton.cs
+++ Assets/Scripts/PanelButton.cs
@@ -5,6 +5,10 @@ public class PanelButton : MonoBehaviour, IPointerClickHandler
 {
     public GameManager.Panels panel;
 
+    public void SetPanel(int newPanel)
+    {
+        panel = (GameManager.Panels)newPanel;
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
