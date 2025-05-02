f3ab4cca99c3d32aaf8c19659876146328a14858
pierregd <pierre.gaudillere@gmail.com>
2025-05-02 16:04:11 +0200

    sons

---
+++ Assets/Scripts/PlayAudioClip.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class PlayAudioClip : MonoBehaviour, IPointerClickHandler
+{
+    public AudioClip clip;
+    public bool playOnClick;
+    
+    public void PlaySound()
+    {
+        if (clip != null)
+            AudioSource.PlayClipAtPoint(clip, FindObjectsByType<AudioListener>(FindObjectsSortMode.None).FirstOrDefault().transform.position);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (playOnClick)
+            PlaySound();
+    }
+}
